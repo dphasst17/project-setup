3dd2c09e470126cc1a1ff7a5f07d78597cff6c07
dphasst17 <phathuu12@gmail.com>
2024-08-26 21:02:42 +0700

    update: create nuxt project

---
+++ nuxt/app.vue
@@ -0,0 +1,10 @@
+import { NuxtPage, NuxtLayout } from './.nuxt/components';
+<script setup lang="tsx">
+</script>
+<template>
+  <div>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
---
+++ nuxt/pages/demo.vue
@@ -0,0 +1,8 @@
+<script setup lang="tsx">
+const nuxtDemo = (a: number, b: number) => {
+  return a + b
+}
+</script>
+<template>
+  <div>demo {{nuxtDemo(1, 2)}}</div>
+</template>
